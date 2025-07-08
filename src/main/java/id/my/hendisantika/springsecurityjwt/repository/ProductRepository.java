845587865c457304161bdfa52cc252c5b03a32f0
hendisantika <hendisantika@yahoo.co.id>
2025-07-09 06:58:15 +0700

    feat: Add ProductRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springsecurityjwt/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.springsecurityjwt.repository;
+
+import id.my.hendisantika.springsecurityjwt.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-Security-JWT
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/07/25
+ * Time: 06.58
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
