0b38c9f9a66a262a3db750e689ee94a4011ede29
hendisantika <hendisantika@yahoo.co.id>
2025-07-09 06:54:58 +0700

    feat: Add RefreshTokenRequest DTO🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springsecurityjwt/dto/RefreshTokenRequest.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.springsecurityjwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-Security-JWT
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/07/25
+ * Time: 06.54
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+public class RefreshTokenRequest {
+    private String refreshToken;
+}
