26a37ae55f698610d3bcf6f3f48f166aa41fa377
hendisantika <hendisantika@yahoo.co.id>
2025-07-09 06:59:23 +0700

    feat: Add AuthService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springsecurityjwt/service/AuthService.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika.springsecurityjwt.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-Security-JWT
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/07/25
+ * Time: 06.59
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+}
