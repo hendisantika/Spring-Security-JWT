4ab14eb0c9f11113c5e92ca12f6762fc6c2549de
hendisantika <hendisantika@yahoo.co.id>
2025-07-09 06:57:09 +0700

    feat: Add Role Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springsecurityjwt/model/Role.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.springsecurityjwt.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-Security-JWT
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/07/25
+ * Time: 06.56
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
