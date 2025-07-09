5824593a3c8a5c4d5dbbbddd433ab31ec5dfa3c4
hendisantika <hendisantika@yahoo.co.id>
2025-07-09 07:07:57 +0700

    feat: Add SecurityConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springsecurityjwt/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.springsecurityjwt.config;
+
+import id.my.hendisantika.springsecurityjwt.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-Security-JWT
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/07/25
+ * Time: 07.07
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+}
