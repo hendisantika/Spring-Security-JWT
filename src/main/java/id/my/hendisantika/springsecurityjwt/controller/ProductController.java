0198f5f5c9bdce716e8be004e8103dc38627559e
hendisantika <hendisantika@yahoo.co.id>
2025-07-09 07:12:20 +0700

    feat: Add deleteProduct in ProductController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springsecurityjwt/controller/ProductController.java
+++ src/main/java/id/my/hendisantika/springsecurityjwt/controller/ProductController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,4 +55,12 @@ public class ProductController {
         product.setId(id);
         return ResponseEntity.ok(productService.saveProduct(product));
     }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        productService.deleteById(id);
+        return ResponseEntity.ok("Product deleted successfully");
+    }
+
 }
