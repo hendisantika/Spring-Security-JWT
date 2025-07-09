1447aa1f4f1f25ca3f309356ef29946cbb014172
hendisantika <hendisantika@yahoo.co.id>
2025-07-09 07:08:25 +0700

    feat: Add passwordEncoder in SecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springsecurityjwt/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/springsecurityjwt/config/SecurityConfig.java
@@ -2,10 +2,13 @@ package id.my.hendisantika.springsecurityjwt.config;
 
 import id.my.hendisantika.springsecurityjwt.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +28,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
