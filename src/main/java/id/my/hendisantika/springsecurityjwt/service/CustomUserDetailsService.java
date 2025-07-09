db2ea76b210d4c077362d92958bf308e3620885e
hendisantika <hendisantika@yahoo.co.id>
2025-07-09 07:01:27 +0700

    feat: Add loadUserByUsername in CustomUserDetailsService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springsecurityjwt/service/CustomUserDetailsService.java
+++ src/main/java/id/my/hendisantika/springsecurityjwt/service/CustomUserDetailsService.java
@@ -1,9 +1,17 @@
 package id.my.hendisantika.springsecurityjwt.service;
 
+import id.my.hendisantika.springsecurityjwt.model.User;
 import id.my.hendisantika.springsecurityjwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +29,21 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                getAuthority(user)
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthority(User user) {
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().name());
+        return List.of(authority);
+    }
 
 }
