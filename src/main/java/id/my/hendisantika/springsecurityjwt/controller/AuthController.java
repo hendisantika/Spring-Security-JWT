3b530a7abd212fdd48c62321a8941989d9aebf36
hendisantika <hendisantika@yahoo.co.id>
2025-07-09 07:10:08 +0700

    feat: Add registerUser in AuthController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springsecurityjwt/controller/AuthController.java
+++ src/main/java/id/my/hendisantika/springsecurityjwt/controller/AuthController.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.springsecurityjwt.controller;
 
+import id.my.hendisantika.springsecurityjwt.dto.RegisterRequest;
 import id.my.hendisantika.springsecurityjwt.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +27,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest request) {
+        // Save the new user to the database and return success response.
+        authService.registerUser(request);
+        return ResponseEntity.ok("User registered successfully");
+    }
 }
