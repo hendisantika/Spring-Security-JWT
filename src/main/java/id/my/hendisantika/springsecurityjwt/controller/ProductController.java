dea6e474a3e8a9c8950a3d5dceed0ebf94fbd6f4
hendisantika <hendisantika@yahoo.co.id>
2025-07-09 07:11:05 +0700

    feat: Add ProductController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springsecurityjwt/controller/ProductController.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.springsecurityjwt.controller;
+
+import id.my.hendisantika.springsecurityjwt.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-Security-JWT
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/07/25
+ * Time: 07.10
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/products")
+@AllArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+}
