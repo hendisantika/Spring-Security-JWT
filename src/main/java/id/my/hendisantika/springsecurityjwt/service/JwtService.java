3b1ded2d4eb01e599489704d4aa1bdeabb7bcef0
hendisantika <hendisantika@yahoo.co.id>
2025-07-09 07:04:30 +0700

    feat: Add getSignInKey in JwtService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springsecurityjwt/service/JwtService.java
+++ src/main/java/id/my/hendisantika/springsecurityjwt/service/JwtService.java
@@ -2,13 +2,17 @@ package id.my.hendisantika.springsecurityjwt.service;
 
 import id.my.hendisantika.springsecurityjwt.dto.TokenPair;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -117,5 +121,10 @@ public class JwtService {
         }
 
         return claims;
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
